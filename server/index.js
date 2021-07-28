71469e12d436041465015b8a2775f3872ddebad2
DESKTOP-6F4CE3E\Shafique <shafique.malik@phenologix.com>
2021-07-29 01:02:01 +0500

    comment change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
  
 
 const app = express();
-//this is deprecated from express version 4.16.0 or later
+//this is deprecated from express version 4.16.0 or later.
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(express.json({limit:"30mb",extended:true}));
